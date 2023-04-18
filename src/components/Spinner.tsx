9d008f10b330221886eb74122f3215faab4ae4fa
Finn <contact@bechinka.eu>
2023-04-18 20:23:28 +0200

    feat: add fetching feedback

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,14 @@
+import "./styles/Spinner.css";
+
+function Spinner() {
+  return (
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Spinner;
